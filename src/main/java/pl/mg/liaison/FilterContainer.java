37022eb2e5c33b216210547154567a1d79895e1a
Maciej Grzeszczak <maciejgrzeszczak@gmail.com>
2016-05-01 12:24:00 +0200

    Removed unnecessary imports

--- src/main/java/pl/mg/liaison/FilterContainer.java
+++ src/main/java/pl/mg/liaison/FilterContainer.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 /**
  * Created by Maciej on 11.04.2016.
